ca34fdcb778a92665b7f95e58c7d0b129a14f574
antonio-cesar.flores <antonio-cesar.flores@IBLAESBARC00675.IBL.INETUM>
2022-09-27 17:20:28 +0200

    Init

---
+++ txt2dcmSR/src/main/java/org/txt2dcmsr/dataElement.java
@@ -0,0 +1,42 @@
+package org.txt2dcmsr;
+
+public class dataElement {
+	String tag;
+	String name;
+	String keyword;
+	String vr;
+	String vm;
+	
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	public String getVr() {
+		return vr;
+	}
+	public void setVr(String vr) {
+		this.vr = vr;
+	}
+	public String getVm() {
+		return vm;
+	}
+	public void setVm(String vm) {
+		this.vm = vm;
+	}
+	
+	
+}
